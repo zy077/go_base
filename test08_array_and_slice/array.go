9706a8aa60ce48627c13b290baa1dfae552775e3
zhouyang <2270962238@qq.com>
2021-07-15 14:12:11 +0800

    数组求和 查找两个元素和等于指定数字并返回他们的下标

--- test08_array_and_slice/array.go
+++ test08_array_and_slice/array.go
@@ -28,6 +28,35 @@ func printArray(array [10]int){
 	fmt.Println("++++++++++++++++++++")
 }
 
+// 数组求和
+func ArraySum(array [10]int){
+	sum01 := 0
+	for i:=0; i<len(array); i++ {
+		sum01 += array[i]
+	}
+	fmt.Println(sum01)
+	fmt.Println("++++++++++++++++++++")
+	var sum02 int
+	for _, item := range array {
+		sum02 += item
+	}
+	fmt.Println(sum02)
+	fmt.Println("++++++++++++++++++++")
+}
+
+// 找出数组中和为指定值的两个元素的下标，比如从数组[1, 3, 5, 7, 8]中找出和为8的两个元素的下标分别为(0,3)和(1,2)。
+func FindIndex(array [5]int, num int) []string {
+	var ret []string
+	for idx01, val01 := range array {
+		for idx02, val02 := range array[idx01+1:]{
+			if val01 + val02 == num {
+				ret = append(ret, fmt.Sprintf("(%d, %d)", idx01, idx01+idx02+1))
+			}
+		}
+	}
+	return ret
+}
+
 
 func main(){
 	array()
@@ -38,4 +67,9 @@ func main(){
 	for _, val := range array03{
 		fmt.Println(val)
 	}
+	// 数组求和
+	ArraySum(array03)
+	array04 := [...]int{1, 3, 5, 7, 8}
+	ret := FindIndex(array04, 8)
+	fmt.Println(ret)
 }
