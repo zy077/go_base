da225dc07c02913fd1a7bbdbd84e7a07fcda06b2
zhouyang <2270962238@qq.com>
2021-07-27 23:17:51 +0800

    更新代码

--- test08_array_and_slice/slice01.go
+++ test08_array_and_slice/slice01.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 // 遍历（动态）数组
 func test01_slice(array []int)  {
@@ -11,6 +14,12 @@ func test01_slice(array []int)  {
 	fmt.Println("++++++++++++++++++")
 }
 
+// 切片排序
+func SliceSort(slice0 []int) []int {
+	sort.Ints(slice0)
+	return slice0
+}
+
 func main()  {
 	// 定义一个动态数组
 	array01 := []int{1,2,3,4}
@@ -19,4 +28,26 @@ func main()  {
 	for _, val := range array01{
 		fmt.Println(val)
 	}
+
+	slice01 := make([]int, 10, 10)
+	slice02 := slice01[4:]
+	fmt.Printf("len: %d, cap: %d", len(slice02), len(slice02))
+	fmt.Println("++++++++++++++++++")
+
+	array02 := [6]int{1,2,3,4,5,6}
+	slice03 := array02[2:]
+	fmt.Printf("len: %d, cap: %d", len(slice03), len(slice03))
+	fmt.Println("++++++++++++++++++")
+
+	a := make([]string, 5, 10)
+	for i:=0; i<10; i++ {
+		a = append(a, fmt.Sprintf("%v", i))
+	}
+	fmt.Println(a)
+	fmt.Println("++++++++++++++++++")
+
+	slice04 := []int{3, 7, 8, 9, 1}
+	slice05 := SliceSort(slice04)
+	fmt.Println(slice05)
+	fmt.Println("++++++++++++++++++")
 }
--- test09_map/map_use.go
+++ test09_map/map_use.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main()  {
 	// 声明一个map
@@ -26,6 +29,12 @@ func main()  {
 	delete(map0, "陕西")
 	forMap(map0)
 
+	// 写一个程序，统计一个字符串中每个单词出现的次数。比如：”how do you do”中how=1 do=2 you=1。
+	WordStr := "how do you do"
+	CountWord(WordStr)
+
+	// 打印
+	PrintStr()
 }
 
 func forMap(myMay map[string]string)  {
@@ -34,4 +43,34 @@ func forMap(myMay map[string]string)  {
 		fmt.Printf("%s = %s\n", key, value)
 	}
 	fmt.Println("===================")
+}
+
+//写一个程序，统计一个字符串中每个单词出现的次数。比如：”how do you do”中how=1 do=2 you=1。
+func CountWord(WordStr string){
+	Wordlist := make([]string, 0, 10)
+	WordMap := make(map[string]int)
+	Wordlist = strings.Split(WordStr, " ")
+	for _, k := range Wordlist{
+		if _, ok := WordMap[k]; ok {
+			WordMap[k] += 1
+		}else{
+			WordMap[k] = 1
+		}
+	}
+	for k, v := range WordMap {
+		fmt.Printf("{%s: %d} \n", k, v)
+	}
+	fmt.Println("===================")
+}
+
+func PrintStr() {
+	type Map map[string][]int
+	m := make(Map)
+	s := []int{1, 2}
+	s = append(s, 3)
+	fmt.Printf("%+v\n", s)
+	m["q1mi"] = s
+	s = append(s[:1], s[2:]...)
+	fmt.Printf("%+v\n", s)
+	fmt.Printf("%+v\n", m["q1mi"])
 }
